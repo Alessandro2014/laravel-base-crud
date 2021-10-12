2f13706ae4bf7cb355c8e9f81f2714cc4d151ea4
Alessandro2014 <alessandromerenda22@gmail.com>
2021-10-12 11:05:51 +0200

    grafica show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,23 +2,23 @@
 
 @section('title', 'Comic')
 
-@section('section-id', "comic-details")
+@section('section-id', 'comic-details')
 
 @section('content')
     {{-- CARD SINGOLO FUMETTO --}}
 
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
-          <div class="col-4">
-            <img src="{{ $comic->thumb }}" class="img-fluid rounded-start" alt="{{ $comic->title }}">
-          </div>
-          <div class="col-8">
-            <div class="card-body">
-              <h1 class="card-title">{{ $comic->title }}</h1>
-              <p class="card-text">{{ $comic->description }}</p>
+            <div class="col-4">
+                <img src="{{ $comic->thumb }}" class="img-fluid rounded-start" alt="{{ $comic->title }}">
             </div>
-          </div>
+            <div class="col-8">
+                <div class="card-body">
+                    <h1 class="card-title">{{ $comic->title }}</h1>
+                    <p class="card-text">{{ $comic->description }}</p>
+                </div>
+            </div>
         </div>
-      </div>
+    </div>
 
 @endsection
