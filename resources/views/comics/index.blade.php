fda4cfa3d1a89e1562ea38eb349c067cce35de18
Alessandro2014 <alessandromerenda22@gmail.com>
2021-10-13 17:21:43 +0200

    correzione chiusura col index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,16 +4,16 @@
 
 @section('content')
     {{-- FORM DI RICERCA --}}
-    <form method="GET" class="d-flex justify-content-center mb-5">
+    <form method="GET" class="d-flex mb-5">
         <input class="form-control me-2" type="text" name="search" placeholder="Cerca un fumetto.."
             value="{{ $search }}" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Cerca</button>
     </form>
-        {{-- CARDS CON FUMETTI --}}
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-            <div class="card" style="width: 16rem;">
-                @forelse ($comics as $comic)
+    {{-- CARDS CON FUMETTI --}}
+    <div class="row">
+        @forelse ($comics as $comic)
+            <div class="col-3">
+                <div class="card" style="width: 16rem;">
                     <img src="{{ $comic->thumb }}" class="card-img-top " alt="{{ $comic->title }}">
                     <div class="card-body">
                         <h5 class="card-title">{{ $comic->title }}</h5>
@@ -27,11 +27,11 @@
                     <div class="card-body text-center">
                         <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-info p-2">Vai alla descrizione</a>
                     </div>
-                @empty
-                    <h6 class="text-center">Nessun fumetto trovato</h6>
-                @endforelse
+                </div>
             </div>
-        </div>
+        @empty
+            <h6 class="text-center">Nessun fumetto trovato</h6>
+        @endforelse
     </div>
 
 @endsection
